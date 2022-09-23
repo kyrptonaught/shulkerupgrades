2f52cd3b8937b3068482e4f35f1d73fb77b5a9d4
quat1024 <quat1024@users.noreply.github.com>
2022-09-22 22:54:17 -0400

    Fix #18 (bugged logic in fillExistingStack)
    Instead of `ItemEntityMixin` copy-pasting much of the code in
    onPlayerCollision, the ItemStack that is to be inserted into the
    player's inventory is modified directly, using a ModifyArg. This didn't
    end up being as clean as I would have liked it to be (needs a supporting
    Inject because of ModifyArg limitations, and there's some wrinkles
    related to putIntoShulker returning a new ItemStack instead of mutating
    the existing one)

--- src/main/java/net/kyrptonaught/upgradedshulker/mixin/ItemEntityMixin.java
+++ src/main/java/net/kyrptonaught/upgradedshulker/mixin/ItemEntityMixin.java
@@ -10,31 +10,18 @@ import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stat.Stats;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.UUID;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin extends Entity {
-
-    @Shadow
-    public abstract ItemStack getStack();
-
-    @Shadow
-    private int pickupDelay;
-
-    @Shadow
-    private UUID owner;
-
     @Shadow
     public abstract void setStack(ItemStack stack);
 
@@ -42,49 +29,43 @@ public abstract class ItemEntityMixin extends Entity {
         super(type, world);
     }
 
-    @Inject(method = "onPlayerCollision", at = @At(value = "HEAD"), cancellable = true)
-    public void attemptHopperPickup(PlayerEntity player, CallbackInfo ci) {
-        if (!this.world.isClient) {
-            ItemStack itemStack = this.getStack();
-            Item item = itemStack.getItem();
-            int i = itemStack.getCount();
-            if (this.pickupDelay == 0 && (this.owner == null || this.owner.equals(player.getUuid()))) {
-                fillExistingStack(player, itemStack, player.getInventory());
-                itemStack = putIntoHopperShulker(player, itemStack, player.getInventory());
-                this.setStack(itemStack);
-                if (itemStack.getCount() != i) {
-                    player.sendPickup(this, i);
-                    if (itemStack.isEmpty()) {
-                        this.discard();
-                        itemStack.setCount(i);
-                        player.increaseStat(Stats.PICKED_UP.getOrCreateStat(item), i);
-                        player.triggerItemPickedUpByEntityCriteria((ItemEntity) (Object) this);
-                        ci.cancel();
-                    } else {
-                        player.increaseStat(Stats.PICKED_UP.getOrCreateStat(item), i);
-                        player.triggerItemPickedUpByEntityCriteria((ItemEntity) (Object) this);
-                    }
-                }
-            }
-        }
+    // Workaround for ModifyArg not being able to capture arguments of the calling method.
+    @Unique private PlayerEntity addingToPlayer;
+
+    @Inject(
+            method = "onPlayerCollision",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;insertStack(Lnet/minecraft/item/ItemStack;)Z")
+    )
+    public void UPGRADEDSHULKER$onPlayerCollision$modifyArgIsWeird(PlayerEntity player, CallbackInfo ci) {
+        addingToPlayer = player;
     }
 
-    private void fillExistingStack(PlayerEntity player, ItemStack pickupStack, PlayerInventory playerInventory) {
-        if (pickupStack.isStackable())
-            for (int i = 0; i < playerInventory.size(); i++) {
-                ItemStack shulker = playerInventory.getStack(i);
-                if (shulker.isStackable() && shulker.getCount() < shulker.getMaxCount() && shulker.isItemEqual(pickupStack)) {
-                    int amount = Math.min(shulker.getMaxCount() - shulker.getCount(), pickupStack.getCount());
-                    shulker.increment(amount);
-                    pickupStack.decrement(amount);
-                    if (pickupStack.getCount() <= 0)
-                        break;
-                }
-            }
+    @ModifyArg(
+            method = "onPlayerCollision",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;insertStack(Lnet/minecraft/item/ItemStack;)Z")
+    )
+    public ItemStack UPGRADEDSHULKER$onPlayerCollision$modifyInsertStack(ItemStack oldStack) {
+        PlayerEntity player = addingToPlayer;
+        addingToPlayer = null;
+        int oldCount = oldStack.getCount();
+
+        ItemStack newStack = putIntoShulker(player, oldStack, player.getInventory());
+
+        // Manually play the pickup animation before calling setStack - else you might get a pickup animation for an empty stack, which just
+        // makes it look like the item vanishes. This looks bad.
+        if(oldCount > 0 && newStack.isEmpty())
+            player.sendPickup((ItemEntity) (Object) this, oldCount);
+
+        // It's possible for putIntoShulker to return a new instance of ItemStack (e.g. ItemStack.EMPTY), instead of mutating oldStack.
+        // If this happens, we should update the ItemEntity to reflect the new stack instance.
+        if(oldStack != newStack)
+            setStack(newStack);
+
+        return newStack;
     }
 
     @Unique
-    private ItemStack putIntoHopperShulker(PlayerEntity player, ItemStack pickupStack, PlayerInventory playerInventory) {
+    private ItemStack putIntoShulker(PlayerEntity player, ItemStack pickupStack, PlayerInventory playerInventory) {
         if (pickupStack.getCount() > 0) {
             for (int i = 0; i < playerInventory.size(); i++) {
                 ItemStack shulker = playerInventory.getStack(i);
